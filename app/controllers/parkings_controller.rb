5cf8e15288dd53c80ddfb783ff11fc151dc27767
Esteban Montantes <esmo.ledev@gmail.com>
2018-11-13 09:10:04 -0500

    create new parking view

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,19 +1,30 @@
 class ParkingsController < ApplicationController
   def index
-  end
-
-  def show
+    @parkings = Parking.all
   end
 
   def new
+    @parking = Parking.new
   end
 
   def create
+    raise
+  end
+
+  def show
   end
 
   def update
   end
 
   def edit
+  end
+
+  def delete
+  end
+
+  private
+  def parking_params
+    params.require(:user).permit(:address, :description, :start_time, :finish_time, :price)
   end
 end
