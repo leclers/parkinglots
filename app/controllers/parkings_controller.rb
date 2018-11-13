69e13a3202c04ad8a172bcb10ccdb3fcb95082a5
Esteban Montantes <esmo.ledev@gmail.com>
2018-11-13 11:42:04 -0500

    propose-new-parking-lot

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,30 +1,37 @@
 class ParkingsController < ApplicationController
+  # parkings references devise/user
+  # will need to validate user before action
+  # current_user OR user_id
+  # USE A PUNDIT PUNDIT PUNDIT
   def index
     @parkings = Parking.all
   end
 
   def new
-    @parking = Parking.new
+    @parking = current_user.parkings.build
   end
 
   def create
-    raise
+    @parking = current_user.parkings.build(parking_params)
+    if @parking.save
+      raise
+      # flash[:success] = "Success! Rental space was created."
+      redirect_to action: 'index'
+    else
+      # flash[:error] = "Woopsy, please verify your information is correct"
+      redirect_to action: 'index'
+    end
   end
 
   def show
-  end
-
-  def update
+    raise
   end
 
   def edit
   end
 
-  def delete
-  end
-
-  private
+  protected
   def parking_params
-    params.require(:user).permit(:address, :description, :start_time, :finish_time, :price)
+    params.require(:parking).permit(:address, :description, :start_time, :finish_time, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :parkings, only: [:index, :show, :new, :create]
   resources :bookings, only: [:index, :show, :new, :create, :update, :edit]
+  resources :parkings, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
