4857f707971c11174f9a9754ceb9e538e5fd3cf8
camille villard <hello@camillevillard.com>
2018-11-13 19:08:47 -0500

    font style

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :update, :edit]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/about', to: 'pages#about'
 end
