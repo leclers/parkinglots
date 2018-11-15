1c4e7e9f9f7cea993fa23aed01c8d38e7ed92b4f
camille villard <hello@camillevillard.com>
2018-11-15 12:09:09 -0500

    style for show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,6 +31,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_time, :finish_time)
   end
 end
