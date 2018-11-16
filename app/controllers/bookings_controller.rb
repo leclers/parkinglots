200fe20929a4d58299ac2a990c420ad52a2402a6
camille villard <hello@camillevillard.com>
2018-11-16 11:51:28 -0500

    updating bookings is now possible

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:edit, :update]
+
   def index
     @requested_bookings = policy_scope(Booking)
     @own_parking_bookings = current_user.own_parkings_bookings
@@ -14,14 +15,11 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
-    # @parking = Parking.find(params[:id])
-    # @bookings = policy_scope(Booking)
     authorize @booking
   end
 
-  # needs an if statement to not allow selecting dates that are not available
   def create
-    @parking = Parking.find(param[:id])
+    @parking = Parking.find(params[:parking_id])
     @booking = Booking.new(booking_params)
     @booking.parking = @parking
     @booking.user = current_user
@@ -41,12 +39,14 @@ class BookingsController < ApplicationController
 
   def update
     authorize @booking
-    if @booking.update(booking_params)
-      flash[:notice] = "successfully edited your booking"
-      redirect_to parking_path(@parking)
+    if @booking.update(booking_params) && @booking.status == "accepted"
+      flash[:notice] = "booking accepted"
+      redirect_to booking_path(@booking)
+    elsif @booking.update(booking_params) && @booking.status == "declined"
+      flash[:alert] = "booking rejected"
+      redirect_to booking_path(@booking)
     else
-      flash[:alert] = "oops, something is wrong, dude."
-      render :edit
+      render :show
     end
   end
 
@@ -57,6 +57,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_time, :finish_time)
+    params.require(:booking).permit(:start_time, :finish_time, :status)
   end
 end
